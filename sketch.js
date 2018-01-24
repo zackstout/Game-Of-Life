469478e0e20df30c0aecf3c34497c7fb75942dc8
Zack Stout <zackstout@gmail.com>
2018-01-23 18:47:09 -0600

    all right we've got our grid

--- sketch.js
+++ sketch.js
@@ -1,12 +1,31 @@
 
-function setup() {
-  createCanvas(800, 800);
 
+
+function setup() {
+  var can = createCanvas(800, 800);
+
+  //not sure why this has to be declared in here....
+  function makeGrid(s) {
+    var w = can.width;
+    var h = can.height;
+    //Say w=100. If s=5, we want start=10, then increment by 20. If s=20, we want start=2.5, and increment by 5.
+    var start = w/(s*2);
+
+    //to be honest, not entirely sure why we need the -50:
+    for (var i = start; i < can.width - 50; i += 2*start) {
+      for (var j = start; j < can.height - 50; j += 2*start) {
+        rect(i, j, 2*start - 1, 2*start - 1);
+      }
+    }
+  }
+  makeGrid(20);
 }
 
 
+
+
 function draw() {
-  background(100);
+  background(0, 150);
   fill();
 
 }
