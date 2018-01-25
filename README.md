b23e735ee8b742f9ef4cd5d5721cfca2d31034a0
Zack Stout <zackstout@gmail.com>
2018-01-24 22:33:23 -0600

    jeez i really cannot figure out what the problem is with this code

---
+++ README.md
@@ -0,0 +1,5 @@
+# Game of Life
+Yep! Let's build the Game of Life!
+
+## Built With
+- p5.js
--- sketch.js
+++ sketch.js
@@ -1,19 +1,46 @@
 
 //i is column, j is row:
-//how terribly strange, it won't color the first one on the screen....:
-var startingCreature = [{i: 1, j: 4}, {i: 1, j: 5}, {i: 2, j: 4}, {i: 1, j: 4}];
+//how terribly strange, it won't color the first one on the screen...has SOMETHING to do with the fact that i was calling fill() AFTER drawing the rect.:
+var startingCreature = [{i: 10, j: 10}, {i: 3, j: 5}, {i: 4, j: 4}, {i: 4, j: 5}, {i: 4, j: 3}, {i: 2, j: 4}];
 var start, w, h;
 var s = 40;
 
 //to keep track of which cells are alive/dead:
 var gridValues = [];
 
+//trick for making variables global:
+var can;
+
+function makeGrid(s) {
+  w = can.width;
+  h = can.height;
+  offset = 1000/s;
+  //Say w=100. If s=5, we want start=10, then increment by 20. If s=20, we want start=2.5, and increment by 5.
+  start = w/(s*2);
+  // console.log(start);
+
+  //to be honest, not entirely sure why we need the -50. Well I suppose it's like going until (arr.length - 1):
+  for (var i = start; i < w - offset; i += 2*start) {
+    var row = [];
+    for (var j = start; j < h - offset; j += 2*start) {
+      fill('lightgray');
+      rect(i, j, 2*start - 2, 2*start - 2);
+
+      //value signifies alive (1) or dead (0):
+      row.push({i: i, j: j, value: 0, index: 2*(i - start) / s, jindex: 2*(j - start) / s});
+    }
+    gridValues.push(row);
+  }
+  // console.log(gridValues);
+}
+
+
 function getNeighbors(x) {
     neighbors = [];
-    // var R = parseInt(r);
-    // var C = parseInt(c);
 
-    console.log(x.index, x.jindex);
+    // if (x.value) {
+    //   console.log(x.index, x.jindex, x.value);
+    // }
     // console.log(startingCreature);
 
     if (x.index > 0) {
@@ -24,7 +51,7 @@ function getNeighbors(x) {
         neighbors.push({i: x.index - 1, j: x.jindex - 1});
       }
 
-      if (x.jindex < s - 1) {
+      if (x.jindex < s - 2) {
         neighbors.push({i: x.index, j: x.jindex + 1});
         neighbors.push({i: x.index - 1, j: x.jindex + 1});
       }
@@ -32,84 +59,65 @@ function getNeighbors(x) {
       if (x.jindex > 0) {
         neighbors.push({i: x.index, j: x.jindex - 1});
       }
-      if (x.jindex < s - 1) {
+      if (x.jindex < s - 2) {
         neighbors.push({i: x.index, j: x.jindex + 1});
       }
     }
 
-    if (x.index < s - 1) {
+    if (x.index < s - 2) {
       neighbors.push({i: x.index + 1, j: x.jindex});
 
       if (x.jindex > 0) {
         neighbors.push({i: x.index + 1, j: x.jindex - 1});
       }
-      if (x.jindex < s - 1) {
+      if (x.jindex < s - 2) {
         neighbors.push({i: x.index + 1, j: x.jindex + 1});
       }
     }
-//
-// //if r>0
-//     if (R>0) {
-//       neighbors.push({row: R-1, col: C});
-//       // neighbors.push({row: r-1, col: c+1});
-//       if (c>0) {
-//         neighbors.push({row: R, col: C-1});
-//         neighbors.push({row: R-1, col: C-1});
-//       }
-//       if (c < s-1) {
-//         neighbors.push({row: R, col: C+1});
-//         neighbors.push({row: R-1, col: C+1});
-//       }
-//     } else {
-//       if (c>0) {
-//         neighbors.push({row: R, col: C-1});
-//       }
-//       if (c < s-1) {
-//         neighbors.push({row: R, col: C+1});
-//       }
-//     }
-//
-// //if r < s - 1
-//     if (r < s-1 ) {
-//       neighbors.push({row: R+1, col:C});
-//
-//       if (c>0) {
-//         neighbors.push({row: R+1, col: C-1});
-//       }
-//       if (c < s-1) {
-//         neighbors.push({row: R+1, col: C+1});
-//       }
-//     }
-//
-//     return neighbors;
-    console.log(neighbors);
+
+    if (x.value) {
+      // console.log(neighbors);
+    }
+    return neighbors;
+  } // end getNeighbors
+
+
+  function liveOrDie(x) {
+    var neighbors = getNeighbors(x);
+    var total = 0;
+
+    neighbors.forEach(function(n) {
+      //n has two properties: i and j. i records its column, and j its row.
+      if (gridValues[n.i][n.j].value == 1 ) {
+        total ++;
+        // console.log('aha', n.i, n.j);
+      }
+    });
+
+    // console.log(total);
+    // console.log(x);
+
+    //It always ends up dead in this situation:
+    if (total < 2 || total > 3) {
+      gridValues[x.index][x.jindex].value = 0;
+    }
+
+    //And it always ends up alive in this situation:
+    if (total == 3) {
+      gridValues[x.index][x.jindex].value = 1;
+    }
+    //And if total == 2, it stays in its current state.
+
+    // console.log(x);
   }
+
+
+
+
+
 
 function setup() {
-  var can = createCanvas(800, 800);
-
-  //not sure why this (as well as the above) has to be declared in here....
-  function makeGrid(s) {
-    w = can.width;
-    h = can.height;
-    offset = 1000/s;
-    //Say w=100. If s=5, we want start=10, then increment by 20. If s=20, we want start=2.5, and increment by 5.
-    start = w/(s*2);
-    // console.log(start);
-
-    //to be honest, not entirely sure why we need the -50. Well I suppose it's like going until (arr.length - 1):
-    for (var i = start; i < w - offset; i += 2*start) {
-      var row = [];
-      for (var j = start; j < h - offset; j += 2*start) {
-        rect(i, j, 2*start - 2, 2*start - 2);
-
-        //value signifies alive (1) or dead (0):
-        row.push({i: i, j: j, value: 0, index: 2*(i - start) / s, jindex: 2*(j - start) / s});
-      }
-      gridValues.push(row);
-    }
-    // console.log(gridValues);
-  }
+  can = createCanvas(800, 800);
 
   makeGrid(s);
 
@@ -118,29 +126,58 @@ function setup() {
     var xOff, yOff;
     xOff = start + item.i * 2*start;
     yOff = start + item.j * 2*start;
+    fill('black');
     rect(xOff, yOff, 2*start - 2, 2*start - 2);
-    fill(0, 200);
+    // fill(0, 200);
 
     gridValues[item.i][item.j].value = 1;
   });
 
-  // console.log(gridValues);
+  console.log(gridValues);
 
-  gridValues.forEach(function(row) {
-    row.forEach(function(x) {
-      getNeighbors(x);
-    });
-  });
+  // gridValues.forEach(function(row) {
+  //   row.forEach(function(x) {
+  //     getNeighbors(x);
+  //   });
+  // });
 
 } //end SETUP
 
 
 
-
-
-
 function draw() {
-  background(0, 50);
-  fill();
+  //whoa, need to have 0 here for it to show cells....And even if you take away, still gives the '2 argument' error...
+  // background(0, 50);
+  //ahh, you must pass something to the fill function, OK:
+  // fill(0, 50);
+
+  // liveOrDie({index: 5, jindex: 4, value: 0});
+
+  makeGrid(s);
+  //
+  // gridValues.forEach(function(row) {
+  //   row.forEach(function(x) {
+  //     // var neighbors = getNeighbors(x);
+  //     liveOrDie(x);
+  //
+  //     xOff = start + x.index * 2*start;
+  //     yOff = start + x.jindex * 2*start;
+  //
+  //     if (gridValues[x.index][x.jindex].value == 1) {
+  //       fill('black');
+  //       rect(xOff, yOff, 2*start - 2, 2*start - 2);
+  //
+  //     }
+  //     else {
+  //       fill('lightgray');
+  //       // rect(xOff, yOff, 2*start - 2, 2*start - 2);
+  //
+  //     }
+  //
+  //   });
+  // });
+
+
+
 
 } //end DRAW
